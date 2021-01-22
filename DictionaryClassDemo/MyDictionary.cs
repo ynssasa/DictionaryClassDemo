ef3539e9f3e536e179a09d92790edfca444b0235
yunus sasa <sasayunus03@gmail.com>
2021-01-22 16:30:21 +0300

    Add project files.

---
+++ DictionaryClassDemo/MyDictionary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryClassDemo
+{
+    class MyDictionary<TKey,TValue>
+    {
+        List<TKey> Keys;
+        List<TValue> Values;
+        
+        public MyDictionary()
+        {
+            Keys = new List<TKey>();
+            Values = new List<TValue>();
+        }
+
+        public void Add(TKey tKey,TValue tValue)
+        {          
+            Keys.Add(tKey);
+            Values.Add(tValue); 
+        }
+
+        public TKey GetKey (TValue value)
+        {
+             int index = Values.IndexOf(value);
+             return Keys[index];
+           
+        }
+
+        public TValue GetValue (TKey key)
+        {
+            int index = Keys.IndexOf(key);
+            return Values[index];
+
+        }
+
+    }
+}
---
+++ DictionaryClassDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DictionaryClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Adana");
+            myDictionary.Add(31, "Hatay");
+            myDictionary.Add(34, "İstanbul");
+            Console.WriteLine(myDictionary.GetValue(1));
+            Console.WriteLine(myDictionary.GetValue(31));
+            Console.WriteLine(myDictionary.GetValue(34));
+            Console.WriteLine(myDictionary.GetKey("Adana"));
+            Console.WriteLine(myDictionary.GetKey("Hatay"));
+            Console.WriteLine(myDictionary.GetKey("İstanbul"));
+            
+        }
+    }
+}
